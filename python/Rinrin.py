f75bd5004baa64b1bcdc2383033cd4019b035ed8
Rinrin.py <67325022+Rinrin0413@users.noreply.github.com>
2021-12-24 23:39:07 +0900

    Create:Rinrin.py

---
+++ python/Rinrin.py
@@ -0,0 +1,11 @@
+Rinrin = {
+    "name": "Rinrin",
+    "age": 15,
+    "gender": "male",
+    "birthday": {
+        "year": 2006, 
+        "month": 4, 
+        "date": 13 
+    },
+    "origin_country": "Japan",
+}
