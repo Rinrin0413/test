b509dd408639fe57e5c95bc9c3e17268a57c04c3
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-08-07 02:26:26 +0900

    🔥 Delete: unnecessary files

--- python/turtle幾何学1.py
+++
@@ -1,8 +0,0 @@
-from turtle import *
-import random
-speed(0)
-width(width=7)
-for i in range(100):
-    right(30)
-    circle(150, steps=8)
-done()
--- python/turtle幾何学4.py
+++
@@ -1,8 +0,0 @@
-from turtle import *
-speed(0)
-for i in range(100):
-    pensize(1)
-    right(90)
-    circle(150, steps=3)
-    circle(150, steps=4)
-done()
